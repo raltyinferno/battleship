f7fdf450a1a7c021dd57a519abfe77ec842848a8
Kai Davids Schell <kdavidsschell@alaska.edu>
2016-02-25 00:33:51 -0900

    added prototype state functions

--- main.lua
+++ main.lua
@@ -5,51 +5,104 @@
 
 -- love .
 
+--constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 Board = require('board')
 local ship_err = false
 local mouse_x = nil
 local mouse_y = nil
 
 --states
-local P1_PLACING = 1
-local P2_PLACING = 2
-local P1_TURN = 3
-local P2_TURN = 4
+local START = 1
+local P1_PLACING = 2
+local P2_PLACING = 3
+local P1_TURN = 4
+local P2_TURN = 5
 
-local function handle_P1_PLACING()
+-----------------
+--state handlers
+-----------------
+--split between functions for the love draw, and love update functions
 
-
+----------
+--placing
+----------
+local function handle_P1_PLACING_UP()
+	
 end
+
+local function handle_P1_PLACING_DRAW()
+	
+end
+
+local function handle_P2_PLACING_UP()
+	
+end
+
+local function handle_P2_PLACING_DRAW()
+	
+end
+-----------
+--shooting
+-----------
+local function handle_P1_SHOOTING_UP()
+	
+end
+
+local function handle_P1_SHOOTING_DRAW()
+	
+end
+
+local function handle_P2_SHOOTING_UP()
+	
+end
+
+local function handle_P2_SHOOTING_DRAW()
+	
+end
+
 -- Configuration
 function love.conf(t)
 	t.title = "Battleship"
 	t.version = "LOVE 0.10.1 (Super Toast)"
-	t.window.width = 800
-	t.window.height = 600
+	t.window.width = SCREEN_WIDTH
+	t.window.height = SCREEN_HEIGHT
 
 	-- For Windows debugging
 	t.console = true
 end
 
+ update_handlers = {}
+ 
+ draw_handlers = {}
+
 function love.load(arg)
 	player1board = Board.new_board()
+	player2board = Board.new_board()
+	
+	player1target = Board.new_board()
+	player2target = Board.new_board()
+	
+	grid  = Board.drawBoard(50, player1board)
+	
 	if pcall(Board.place_ship(player1board,1,6,"carrier","vertical")) then
 	else
 		ship_err = true
 	end
-	grid  = Board.drawBoard(50, player1board)
+	
 end
 
--- Board.place_ship(board, x, y, ship_type, orientation)
 function love.update(dt)
+
+--Debuging stuff
 	mouse_x, mouse_y = love.mouse.getPosition()
 end
 
 function love.draw(dt)
-    --love.graphics.print("Hello World",0,0)
-    --love.graphics.rectangle("fill",20,20,10,10)
-    --love.graphics.print(player1board[1][1], 750,550)
 
+--Debuging stuff
     love.graphics.draw(grid, 10,10)
 	love.graphics.print("Mouse x: " .. mouse_x .. " Mouse y " .. mouse_y, 50, 550)
 	if ship_err then
