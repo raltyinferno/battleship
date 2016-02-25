1a26d86dac68971b9539ff9b5d93f8d2e2a4c688
Lonny Strunk <lstrunk2@alaska.edu>
2016-02-24 16:14:02 -0900

    Added a drawBoard function to main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,39 @@
 
 -- love .
 
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+function drawBoard()
+	canvas = love.graphics.newCanvas(100,100)
+
+	love.graphics.setCanvas(canvas)
+		for i = 0, 10, 1 do
+			love.graphics.line(0, i * 10, 10 * 10, i * 10)
+			love.graphics.line(i * 10, 0, i * 10, 10 * 10)
+		end
+	love.graphics.setCanvas()
+	return canvas
 end
+
+-- Configuration
+function love.conf(t)
+	t.title = "Battleship"
+	t.version = "LOVE 0.10.1 (Super Toast)"
+	t.window.width = 800
+	t.window.height = 600
+
+	-- For Windows debugging
+	t.console = true
+end
+
+function love.load(arg)
+	grid  = drawBoard()
+end
+
+
+
+function love.draw(dt)
+    love.graphics.print("Hello World",0,0)
+    love.graphics.rectangle("fill",20,20,10,10)
+
+    love.graphics.draw(grid, 400,300)
+end
+
